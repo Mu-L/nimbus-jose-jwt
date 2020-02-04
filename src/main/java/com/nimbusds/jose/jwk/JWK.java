506adf1ea367456287beb69a5577a2ef831dc747
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2020-02-04 16:43:00 +0200

    Removes redundant PEM parsing in JWK.parseFromPEMEncodedObjects (iss #344)

--- src/main/java/com/nimbusds/jose/jwk/JWK.java
+++ src/main/java/com/nimbusds/jose/jwk/JWK.java
@@ -729,7 +729,7 @@ public abstract class JWK implements JSONAware, Serializable {
 			throw new JOSEException("No PEM-encoded keys found");
 		}
 
-		final KeyPair pair = mergeKeyPairs(toKeyPairList(pemEncodedObjects));
+		final KeyPair pair = mergeKeyPairs(keys);
 
 		final PublicKey publicKey = pair.getPublic();
 		final PrivateKey privateKey = pair.getPrivate();
@@ -804,15 +804,6 @@ public abstract class JWK implements JSONAware, Serializable {
 			throw new JOSEException("Expected key or pair of PEM-encoded keys");
 		}
 		return pair;
-	}
-
-	
-	private static List<KeyPair> toKeyPairList(final String pem) throws JOSEException {
-		final List<KeyPair> keys = PEMEncodedKeyParser.parseKeys(pem);
-		if (keys.isEmpty()) {
-			throw new JOSEException("No PEM-encoded keys found");
-		}
-		return keys;
 	}
 
 	
