7c02d170315ac339c8b9c4b6eb8b11f10efd2503
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2021-09-26 11:13:23 +0300

    Fixes typo in XC20P JOSEException message, adds extra XC20P tests

--- src/main/java/com/nimbusds/jose/crypto/impl/XC20P.java
+++ src/main/java/com/nimbusds/jose/crypto/impl/XC20P.java
@@ -17,136 +17,141 @@
 
 package com.nimbusds.jose.crypto.impl;
 
+
 import com.google.crypto.tink.subtle.XChaCha20Poly1305;
+
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.util.ByteUtils;
 import com.nimbusds.jose.util.Container;
+
 import net.jcip.annotations.ThreadSafe;
 
 import javax.crypto.*;
 import java.security.*;
 
+
 /**
- * This class defines the XChaCha20 stream cipher as well as the use of the Poly1305 authenticator.
+ * This class defines the XChaCha20 stream cipher as well as the use of the
+ * Poly1305 authenticator.
  *
- * The eXtended-nonce ChaCha cipher construction (XChaCha) allows for
+ * <p>The eXtended-nonce ChaCha cipher construction (XChaCha) allows for
  * ChaCha-based cipher suites to accept a 192-bit nonce with similar guarantees
  * to the original construction, except with a much lower probability of nonce
  * misuse occurring.
  *
  * <p>This class is thread-safe.
  *
- * @see <a href="https://datatracker.ietf.org/doc/html/draft-arciszewski-xchacha-03">XChaCha:
- * eXtended-nonce ChaCha and AEAD_XChaCha20_Poly1305</a>
- *
- *
  * @author Alexander Martynov
  * @version 2021-08-04
+ * @see <a href="https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-xchacha-03">XChaCha:
+ * eXtended-nonce ChaCha and AEAD_XChaCha20_Poly1305</a>
  */
 @ThreadSafe
 public class XC20P {
-
-    /**
-     * The standard authentication tag length (128 bits).
-     */
-    public static final int AUTH_TAG_BIT_LENGTH = 128;
-
-    /**
-     * The standard Initialisation Vector (IV) length (192 bits).
-     */
-    public static final int IV_BIT_LENGTH = 192;
-
-
-    /**
-     * Encrypts the specified plain text using XChaCha20_Poly1305.
-     *
-     * @param secretKey   The AES key. Must not be {@code null}.
-     * @param plainText   The plain text. Must not be {@code null}.
-     * @param ivContainer The initialisation vector (IV).
-     *                    This is output parameter. On output, it carries
-     *                    the nonce the cipher actually used.
-     * @param authData    The authenticated data. Must not be {@code null}.
-     *
-     * @return The authenticated cipher text.
-     *
-     * @throws JOSEException If encryption failed.
-     */
-    public static AuthenticatedCipherText encryptAuthenticated(final SecretKey secretKey,
-                                        final Container<byte[]> ivContainer,
-                                        final byte[] plainText,
-                                        final byte[] authData)
-            throws JOSEException {
-
-        final XChaCha20Poly1305 aead;
-
-        try {
-            aead = new XChaCha20Poly1305(secretKey.getEncoded());
-
-        } catch (InvalidKeyException e) {
-            throw new JOSEException("Invalid XChaCha20Poly1305 key: " + e.getMessage(), e);
-        }
-
-        final byte[] cipherOutput;
-
-        try {
-            cipherOutput = aead.encrypt(plainText, authData);
-
-        } catch (GeneralSecurityException e) {
-            throw new JOSEException("Couldn't encrypt with XChaCha20Poly1305: " + e.getMessage(), e);
-        }
-
-        final int tagPos = cipherOutput.length - ByteUtils.byteLength(AUTH_TAG_BIT_LENGTH);
-        final int cipherTextPos = ByteUtils.byteLength(IV_BIT_LENGTH);
-
-        byte[] iv = ByteUtils.subArray(cipherOutput, 0, cipherTextPos);
-        byte[] cipherText = ByteUtils.subArray(cipherOutput, cipherTextPos, tagPos - cipherTextPos);
-        byte[] authTag = ByteUtils.subArray(cipherOutput, tagPos, ByteUtils.byteLength(AUTH_TAG_BIT_LENGTH));
-
-        // set nonce
-        ivContainer.set(iv);
-
-        return new AuthenticatedCipherText(cipherText, authTag);
-    }
-
-
-    /**
-     * Decrypts the specified cipher text using XChaCha20_Poly1305.
-     *
-     * @param secretKey  The AES key. Must not be {@code null}.
-     * @param iv         The initialisation vector (IV). Must not be
-     *                   {@code null}.
-     * @param cipherText The cipher text. Must not be {@code null}.
-     * @param authData   The authenticated data. Must not be {@code null}.
-     * @param authTag    The authentication tag. Must not be {@code null}.
-     *
-     * @return The decrypted plain text.
-     *
-     * @throws JOSEException If decryption failed.
-     */
-    public static byte[] decryptAuthenticated(final SecretKey secretKey,
-                                 final byte[] iv,
-                                 final byte[] cipherText,
-                                 final byte[] authData,
-                                 final byte[] authTag)
-            throws JOSEException {
-
-        final XChaCha20Poly1305 aead;
-
-        try {
-            aead = new XChaCha20Poly1305(secretKey.getEncoded());
-
-        } catch (InvalidKeyException e) {
-            throw new JOSEException("Invalid XChaCha20Poly1305 key: " + e.getMessage(), e);
-        }
-
-        final byte[] cipherInput = ByteUtils.concat(iv, cipherText, authTag);
-
-        try {
-            return aead.decrypt(cipherInput, authData);
-
-        } catch (GeneralSecurityException e) {
-
-            throw new JOSEException("XChaCha20Poly1305decryption failed: " + e.getMessage(), e);
-        }
-    }
+	
+	
+	/**
+	 * The standard authentication tag length (128 bits).
+	 */
+	public static final int AUTH_TAG_BIT_LENGTH = 128;
+	
+	
+	/**
+	 * The standard Initialisation Vector (IV) length (192 bits).
+	 */
+	public static final int IV_BIT_LENGTH = 192;
+	
+	
+	/**
+	 * Encrypts the specified plain text using XChaCha20_Poly1305.
+	 *
+	 * @param secretKey   The AES key. Must not be {@code null}.
+	 * @param plainText   The plain text. Must not be {@code null}.
+	 * @param ivContainer The initialisation vector (IV).
+	 *                    This is output parameter. On output, it carries
+	 *                    the nonce the cipher actually used.
+	 * @param authData    The authenticated data. Must not be {@code null}.
+	 *
+	 * @return The authenticated cipher text.
+	 *
+	 * @throws JOSEException If encryption failed.
+	 */
+	public static AuthenticatedCipherText encryptAuthenticated(final SecretKey secretKey,
+								   final Container<byte[]> ivContainer,
+								   final byte[] plainText,
+								   final byte[] authData)
+		throws JOSEException {
+		
+		final XChaCha20Poly1305 aead;
+		
+		try {
+			aead = new XChaCha20Poly1305(secretKey.getEncoded());
+			
+		} catch (InvalidKeyException e) {
+			throw new JOSEException("Invalid XChaCha20Poly1305 key: " + e.getMessage(), e);
+		}
+		
+		final byte[] cipherOutput;
+		
+		try {
+			cipherOutput = aead.encrypt(plainText, authData);
+			
+		} catch (GeneralSecurityException e) {
+			throw new JOSEException("Couldn't encrypt with XChaCha20Poly1305: " + e.getMessage(), e);
+		}
+		
+		final int tagPos = cipherOutput.length - ByteUtils.byteLength(AUTH_TAG_BIT_LENGTH);
+		final int cipherTextPos = ByteUtils.byteLength(IV_BIT_LENGTH);
+		
+		byte[] iv = ByteUtils.subArray(cipherOutput, 0, cipherTextPos);
+		byte[] cipherText = ByteUtils.subArray(cipherOutput, cipherTextPos, tagPos - cipherTextPos);
+		byte[] authTag = ByteUtils.subArray(cipherOutput, tagPos, ByteUtils.byteLength(AUTH_TAG_BIT_LENGTH));
+		
+		// set nonce
+		ivContainer.set(iv);
+		
+		return new AuthenticatedCipherText(cipherText, authTag);
+	}
+	
+	
+	/**
+	 * Decrypts the specified cipher text using XChaCha20_Poly1305.
+	 *
+	 * @param secretKey  The AES key. Must not be {@code null}.
+	 * @param iv         The initialisation vector (IV). Must not be
+	 *                   {@code null}.
+	 * @param cipherText The cipher text. Must not be {@code null}.
+	 * @param authData   The authenticated data. Must not be {@code null}.
+	 * @param authTag    The authentication tag. Must not be {@code null}.
+	 *
+	 * @return The decrypted plain text.
+	 *
+	 * @throws JOSEException If decryption failed.
+	 */
+	public static byte[] decryptAuthenticated(final SecretKey secretKey,
+						  final byte[] iv,
+						  final byte[] cipherText,
+						  final byte[] authData,
+						  final byte[] authTag)
+		throws JOSEException {
+		
+		final XChaCha20Poly1305 aead;
+		
+		try {
+			aead = new XChaCha20Poly1305(secretKey.getEncoded());
+			
+		} catch (InvalidKeyException e) {
+			throw new JOSEException("Invalid XChaCha20Poly1305 key: " + e.getMessage(), e);
+		}
+		
+		final byte[] cipherInput = ByteUtils.concat(iv, cipherText, authTag);
+		
+		try {
+			return aead.decrypt(cipherInput, authData);
+			
+		} catch (GeneralSecurityException e) {
+			
+			throw new JOSEException("XChaCha20Poly1305 decryption failed: " + e.getMessage(), e);
+		}
+	}
 }
--- src/test/java/com/nimbusds/jose/crypto/impl/XC20PTest.java
+++ src/test/java/com/nimbusds/jose/crypto/impl/XC20PTest.java
@@ -19,11 +19,14 @@ package com.nimbusds.jose.crypto.impl;
 
 
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 
 import com.nimbusds.jose.EncryptionMethod;
 import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.util.ByteUtils;
 import com.nimbusds.jose.util.Container;
+
 import junit.framework.TestCase;
 
 import javax.crypto.SecretKey;
@@ -37,103 +40,242 @@ import static org.junit.Assert.assertArrayEquals;
  * vectors from draft-irtf-cfrg-xchacha-03, appendix A.
  *
  * @author Alexander Martynov
- * @version 2021-08-04
+ * @author Vladimir Dzhuvinov
+ * @version 2021-09-26
  */
 public class XC20PTest extends TestCase {
-
-    private static byte[] fromHex(String hex) {
-        byte[] result = new byte[hex.length() / 2];
-        for (int i=0; i<result.length; i++) {
-            result[i] = (byte) Integer.parseInt(hex.substring(2*i, 2*i + 2), 16);
-        }
-        return result;
-    }
-
-    private static class TestVector {
-        public byte[] iv;
-        public byte[] key;
-        public byte[] tag;
-        public byte[] aad;
-        public byte[] plaintext;
-        public byte[] ciphertext;
-
-        public TestVector(byte[] iv,
-                          byte[] key,
-                          byte[] aad,
-                          byte[] tag,
-                          byte[] plaintext,
-                          byte[] ciphertext) {
-            this.iv = iv;
-            this.key = key;
-            this.tag = tag;
-            this.aad = aad;
-            this.plaintext = plaintext;
-            this.ciphertext = ciphertext;
-        }
-    }
-
-    public void testEncryptDecrypt() throws JOSEException {
-        SecureRandom secureRandom = new SecureRandom();
-        String plainText = "Hello, world!";
-
-
-        // secret key
-        SecretKey key = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
-
-        // aad
-        byte[] aad = new byte[128];
-        secureRandom.nextBytes(aad);
-
-        // IV
-        Container<byte[]> ivContainer = new Container<>(null);
-
-        AuthenticatedCipherText authenticatedCipherText = XC20P.encryptAuthenticated(
-                key,
-                ivContainer,
-                plainText.getBytes(StandardCharsets.UTF_8),
-                aad
-        );
-
-        byte[] decrypted = XC20P.decryptAuthenticated(
-                key,
-                ivContainer.get(),
-                authenticatedCipherText.getCipherText(),
-                aad,
-                authenticatedCipherText.getAuthenticationTag()
-        );
-
-        String clearText = new String(decrypted, StandardCharsets.UTF_8);
-        assertEquals(plainText, clearText);
-    }
-
-    /**
-     * see https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-xchacha-03#appendix-A.3.1
-     */
-    public void test_TestVector() throws JOSEException {
-        TestVector tv = new TestVector(
-                fromHex("404142434445464748494a4b4c4d4e4f5051525354555657"),
-                fromHex("808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"),
-                fromHex("50515253c0c1c2c3c4c5c6c7"),
-                fromHex("c0875924c1c7987947deafd8780acf49"),
-                fromHex("4c616469657320616e642047656e746c656d656e206f662074686520636c6173" +
-                        "73206f66202739393a204966204920636f756c64206f6666657220796f75206f" +
-                        "6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73" +
-                        "637265656e20776f756c642062652069742e"),
-                fromHex("bd6d179d3e83d43b9576579493c0e939572a1700252bfaccbed2902c21396cbb" +
-                        "731c7f1b0b4aa6440bf3a82f4eda7e39ae64c6708c54c216cb96b72e1213b452" +
-                        "2f8c9ba40db5d945b11b69b982c1bb9e3f3fac2bc369488f76b2383565d3fff9" +
-                        "21f9664c97637da9768812f615c68b13b52e"));
-
-        SecretKey key = new SecretKeySpec(tv.key, "AES");
-
-        byte[] decrypted = XC20P.decryptAuthenticated(
-                key,
-                tv.iv,
-                tv.ciphertext,
-                tv.aad,
-                tv.tag
-        );
-
-        assertArrayEquals(tv.plaintext, decrypted);
-    }
+	
+	
+	private static byte[] fromHex(String hex) {
+		byte[] result = new byte[hex.length() / 2];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+		}
+		return result;
+	}
+	
+	
+	private static class TestVector {
+		
+		public byte[] iv;
+		
+		public byte[] key;
+		
+		public byte[] tag;
+		
+		public byte[] aad;
+		
+		public byte[] plaintext;
+		
+		public byte[] ciphertext;
+		
+		public TestVector(byte[] iv,
+				  byte[] key,
+				  byte[] aad,
+				  byte[] tag,
+				  byte[] plaintext,
+				  byte[] ciphertext) {
+			this.iv = iv;
+			this.key = key;
+			this.tag = tag;
+			this.aad = aad;
+			this.plaintext = plaintext;
+			this.ciphertext = ciphertext;
+		}
+	}
+	
+	
+	public void testEncryptDecrypt() throws JOSEException {
+		
+		SecureRandom secureRandom = new SecureRandom();
+		
+		String plainText = "Hello, world!";
+		
+		// generate secret key
+		SecretKey key = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
+		
+		// generate aad
+		byte[] aad = new byte[128];
+		secureRandom.nextBytes(aad);
+		
+		// IV
+		Container<byte[]> ivContainer = new Container<>(null);
+		
+		AuthenticatedCipherText authenticatedCipherText = XC20P.encryptAuthenticated(
+			key,
+			ivContainer,
+			plainText.getBytes(StandardCharsets.UTF_8),
+			aad
+		);
+		
+		byte[] decrypted = XC20P.decryptAuthenticated(
+			key,
+			ivContainer.get(),
+			authenticatedCipherText.getCipherText(),
+			aad,
+			authenticatedCipherText.getAuthenticationTag()
+		);
+		
+		String clearText = new String(decrypted, StandardCharsets.UTF_8);
+		assertEquals(plainText, clearText);
+	}
+	
+	
+	public void testDecryptException_modifiedCipherText() throws JOSEException {
+		
+		SecureRandom secureRandom = new SecureRandom();
+		
+		String plainText = "Hello, world!";
+		
+		// generate secret key
+		SecretKey key = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
+		
+		// generate aad
+		byte[] aad = new byte[128];
+		secureRandom.nextBytes(aad);
+		
+		// IV
+		Container<byte[]> ivContainer = new Container<>(null);
+		
+		AuthenticatedCipherText authenticatedCipherText = XC20P.encryptAuthenticated(
+			key,
+			ivContainer,
+			plainText.getBytes(StandardCharsets.UTF_8),
+			aad
+		);
+		
+		byte[] modifiedCipherText = new byte[authenticatedCipherText.getCipherText().length];
+		secureRandom.nextBytes(modifiedCipherText);
+		
+		try {
+			XC20P.decryptAuthenticated(
+				key,
+				ivContainer.get(),
+				modifiedCipherText,
+				aad,
+				authenticatedCipherText.getAuthenticationTag());
+			fail();
+		} catch (JOSEException e) {
+			assertEquals("XChaCha20Poly1305 decryption failed: java.security.GeneralSecurityException: invalid MAC", e.getMessage());
+			assertTrue(e.getCause() instanceof GeneralSecurityException);
+			assertEquals("java.security.GeneralSecurityException: invalid MAC", e.getCause().getMessage());
+		}
+	}
+	
+	
+	public void testDecryptException_modifiedAuthenticationTag() throws JOSEException {
+		
+		SecureRandom secureRandom = new SecureRandom();
+		
+		String plainText = "Hello, world!";
+		
+		// generate secret key
+		SecretKey key = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
+		
+		// generate aad
+		byte[] aad = new byte[128];
+		secureRandom.nextBytes(aad);
+		
+		// IV
+		Container<byte[]> ivContainer = new Container<>(null);
+		
+		AuthenticatedCipherText authenticatedCipherText = XC20P.encryptAuthenticated(
+			key,
+			ivContainer,
+			plainText.getBytes(StandardCharsets.UTF_8),
+			aad
+		);
+		
+		assertEquals(XC20P.AUTH_TAG_BIT_LENGTH, ByteUtils.bitLength(authenticatedCipherText.getAuthenticationTag()));
+		
+		byte[] modifiedAuthenticationTag = new byte[XC20P.AUTH_TAG_BIT_LENGTH];
+		secureRandom.nextBytes(modifiedAuthenticationTag);
+		
+		try {
+			XC20P.decryptAuthenticated(
+				key,
+				ivContainer.get(),
+				authenticatedCipherText.getCipherText(),
+				aad,
+				modifiedAuthenticationTag);
+			fail();
+		} catch (JOSEException e) {
+			assertEquals("XChaCha20Poly1305 decryption failed: java.security.GeneralSecurityException: invalid MAC", e.getMessage());
+			assertTrue(e.getCause() instanceof GeneralSecurityException);
+			assertEquals("java.security.GeneralSecurityException: invalid MAC", e.getCause().getMessage());
+		}
+	}
+	
+	
+	public void testDecryptException_keyMismatch() throws JOSEException {
+		
+		SecureRandom secureRandom = new SecureRandom();
+		
+		String plainText = "Hello, world!";
+		
+		// generate secret key
+		SecretKey key = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
+		
+		// generate aad
+		byte[] aad = new byte[128];
+		secureRandom.nextBytes(aad);
+		
+		// IV
+		Container<byte[]> ivContainer = new Container<>(null);
+		
+		AuthenticatedCipherText authenticatedCipherText = XC20P.encryptAuthenticated(
+			key,
+			ivContainer,
+			plainText.getBytes(StandardCharsets.UTF_8),
+			aad
+		);
+		
+		SecretKey otherKey = ContentCryptoProvider.generateCEK(EncryptionMethod.XC20P, secureRandom);
+		
+		try {
+			XC20P.decryptAuthenticated(
+				otherKey,
+				ivContainer.get(),
+				authenticatedCipherText.getCipherText(),
+				aad,
+				authenticatedCipherText.getAuthenticationTag());
+			fail();
+		} catch (JOSEException e) {
+			assertEquals("XChaCha20Poly1305 decryption failed: java.security.GeneralSecurityException: invalid MAC", e.getMessage());
+			assertTrue(e.getCause() instanceof GeneralSecurityException);
+			assertEquals("java.security.GeneralSecurityException: invalid MAC", e.getCause().getMessage());
+		}
+	}
+	
+	
+	/**
+	 * see https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-xchacha-03#appendix-A.3.1
+	 */
+	public void test_TestVector() throws JOSEException {
+		TestVector tv = new TestVector(
+			fromHex("404142434445464748494a4b4c4d4e4f5051525354555657"),
+			fromHex("808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"),
+			fromHex("50515253c0c1c2c3c4c5c6c7"),
+			fromHex("c0875924c1c7987947deafd8780acf49"),
+			fromHex("4c616469657320616e642047656e746c656d656e206f662074686520636c6173" +
+				"73206f66202739393a204966204920636f756c64206f6666657220796f75206f" +
+				"6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73" +
+				"637265656e20776f756c642062652069742e"),
+			fromHex("bd6d179d3e83d43b9576579493c0e939572a1700252bfaccbed2902c21396cbb" +
+				"731c7f1b0b4aa6440bf3a82f4eda7e39ae64c6708c54c216cb96b72e1213b452" +
+				"2f8c9ba40db5d945b11b69b982c1bb9e3f3fac2bc369488f76b2383565d3fff9" +
+				"21f9664c97637da9768812f615c68b13b52e"));
+		
+		SecretKey key = new SecretKeySpec(tv.key, "AES");
+		
+		byte[] decrypted = XC20P.decryptAuthenticated(
+			key,
+			tv.iv,
+			tv.ciphertext,
+			tv.aad,
+			tv.tag
+		);
+		
+		assertArrayEquals(tv.plaintext, decrypted);
+	}
 }
