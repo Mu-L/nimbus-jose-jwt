b71b67736c1e5ccaac490a45e2dba0f07bb2eeab
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2021-10-09 14:02:56 +0300

    Adds JWS JSON example for online docs

--- src/test/java/com/nimbusds/jose/JWSObjectJSONTest.java
+++ src/test/java/com/nimbusds/jose/JWSObjectJSONTest.java
@@ -25,12 +25,10 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 import com.nimbusds.jose.crypto.*;
-import com.nimbusds.jose.jwk.Curve;
-import com.nimbusds.jose.jwk.ECKey;
-import com.nimbusds.jose.jwk.OctetKeyPair;
-import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.*;
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import com.nimbusds.jose.jwk.gen.OctetKeyPairGenerator;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import com.nimbusds.jose.util.Base64URL;
 import com.nimbusds.jose.util.JSONObjectUtils;
 
@@ -779,6 +777,86 @@ public class JWSObjectJSONTest extends TestCase {
 			fail();
 		} catch (ParseException e) {
 			assertEquals("The parameters in the JWS protected header and the unprotected header must be disjoint", e.getMessage());
+		}
+	}
+	
+	
+	public void testForDocExample_generalWithTwoSignatures()
+		throws Exception {
+		
+		// Generate two signing keys in JWK format
+		
+		// 2048-bit RSA signing key for RS256 alg
+		RSAKey rsaJWK = new RSAKeyGenerator(2048)
+			.algorithm(JWSAlgorithm.RS256)
+			.keyUse(KeyUse.SIGNATURE)
+			.keyID("1")
+			.generate();
+		
+		// EC signing key with P-256 curve for ES256 alg
+		ECKey ecJWK = new ECKeyGenerator(Curve.P_256)
+			.algorithm(JWSAlgorithm.ES256)
+			.keyUse(KeyUse.SIGNATURE)
+			.keyID("2")
+			.generate();
+		
+		// The payload to sign
+		Payload payload = new Payload("Hello, world!");
+		
+		// Create the JWS secured object for JSON serialisation
+		JWSObjectJSON jwsObjectJSON = new JWSObjectJSON(payload);
+		
+		// Apply the first signature with the RSA key
+		jwsObjectJSON.sign(
+			new JWSHeader.Builder((JWSAlgorithm) rsaJWK.getAlgorithm())
+				.keyID(rsaJWK.getKeyID())
+				.build(),
+			new RSASSASigner(rsaJWK)
+		);
+		
+		// Apply the second signature with the EC key
+		jwsObjectJSON.sign(
+			new JWSHeader.Builder((JWSAlgorithm) ecJWK.getAlgorithm())
+				.keyID(ecJWK.getKeyID())
+				.build(),
+			new ECDSASigner(ecJWK)
+		);
+		
+		// Serialise to JSON
+		String json = jwsObjectJSON.serializeGeneral();
+		
+		// Get the public keys to allow recipients to verify
+		// the signatures
+		RSAKey rsaPublicJWK = rsaJWK.toPublicJWK();
+		ECKey ecPublicJWK = ecJWK.toPublicJWK();
+		
+//		System.out.println(json);
+		
+		// Parse the JWS JSON
+		jwsObjectJSON = JWSObjectJSON.parse(json);
+		
+		// Verify the signatures with the available public JWKSs
+		for (JWSObjectJSON.Signature sig: jwsObjectJSON.getSignatures()) {
+			
+			// The JWS kid header parameter is used to identify the signing key
+			
+			if (rsaPublicJWK.getKeyID().equals(sig.getHeader().getKeyID())) {
+				if (! sig.verify(new RSASSAVerifier(rsaPublicJWK))) {
+					System.out.println("Invalid RSA signature for key " + rsaPublicJWK.getKeyID());
+				}
+			}
+			
+			if (ecPublicJWK.getKeyID().equals(sig.getHeader().getKeyID())) {
+				if (! sig.verify(new ECDSAVerifier(ecPublicJWK))) {
+					System.out.println("Invalid EC signature for key " + ecJWK.getKeyID());
+				}
+			}
+		}
+		
+		if (JWSObjectJSON.State.VERIFIED.equals(jwsObjectJSON.getState())) {
+			System.out.println("JWS JSON verified");
+		} else {
+			System.out.println("JWS JSON invalid");
 		}
 	}
 }
